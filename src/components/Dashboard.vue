35a1e55957e47b5b7829aaccf61b0b859dfe7664
dartz09x@gmail.com <dartz09x@gmail.com>
2020-01-18 00:08:48 +0800

    Initial Upload

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+	bContainer
+		bRow
+			div testing
+</template>
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+export default {
+	name: 'Dashboard',
+	created : function(){
+		var h = Vue.axios({
+		  method: 'get',
+		  url: 'https://api.arthuronline.co.uk/v2/properties?Property_Type=Mixed',
+		  headers: {'Authorization': 'Bearer 747323e49e1b6cb36fe857266c2ddca3906532d34cd19ba3507e86f5f5f48c33',
+			'X-EntityID' : '89441'}
+		}).then(function(response) {
+			console.log(response)
+		})
+		
+	}
+}
+</script>
